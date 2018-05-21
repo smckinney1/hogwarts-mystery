5b3d5631b7b09069130af6f3846b64cd8fe738e3
smckinney <smckinney@pltw.org>
2018-05-21 15:41:05 -0400

    JS and CSS files hashed in final bundles

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,40 @@
 const { resolve } = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 // We are setting an environmental variable
 // To run from custom port, run command port=[any other port] npm start
 const port = process.env.PORT || 8080;
 // console.log(process.env);
 
-// We're setting the MODE in package.json
 // __dirname: directory name for the webpack config file (needs ABSOLUTE path)
-// dev.contentBase: where files are served from
-// module.rules.use: Order matters (LAST to FIRST)
+// module.rules[n].use: Order matters (LAST to FIRST)
 module.exports = {
-  // mode: 'development',
   entry: './src/index.js',
   output: {
-    filename: 'bundle.js',
+    filename: '[name].[chunkhash].js',
     path: resolve(__dirname, 'dist'),
   },
   module: {
     rules: [
       { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
-      { test: /\.scss$/, exclude: /node_modules/, use: [ 'style-loader', 'css-loader', 'sass-loader' ] },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: [ 'style-loader', MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader' ],
+      },
     ],
   },
   devServer: {
     port,
   },
   plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'style.[contenthash].css',
+    }),
     new HtmlWebpackPlugin({
-      template: 'src/index.html'
+      template: 'src/index.html',
+      hash: true,
     })
   ],
 }
